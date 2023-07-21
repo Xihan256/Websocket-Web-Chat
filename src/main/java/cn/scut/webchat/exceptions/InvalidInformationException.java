b047b6d0d8a487a3a549f3c965aaa32ca7a8f25a
xihan <77563304+Xihan256@users.noreply.github.com>
2023-07-21 12:30:16 +0800

    Delete src directory

--- src/main/java/cn/scut/webchat/exceptions/InvalidInformationException.java
+++
@@ -1,8 +0,0 @@
-package cn.scut.webchat.exceptions;
-
-public class InvalidInformationException extends Exception{
-
-    public InvalidInformationException(String message) {
-        super(message + "回去改改吧");
-    }
-}
